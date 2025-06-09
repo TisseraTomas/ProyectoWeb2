b2a0b1eb0b7a246c85eb0a65e7aece52fb4ad136
TisseraTomas <tomyagustin1110@gmail.com>
2025-06-09 09:29:42 -0300

    BD

--- tienda_ropa.sql
+++ tienda_ropa.sql
@@ -16,6 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `productos`
 --
 
@@ -24,6 +49,7 @@ DROP TABLE IF EXISTS `productos`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `productos` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `producto_padre_id` int DEFAULT NULL,
   `nombre` varchar(100) DEFAULT NULL,
   `categoria` varchar(100) DEFAULT NULL,
   `precio` decimal(10,2) DEFAULT NULL,
@@ -32,9 +58,10 @@ CREATE TABLE `productos` (
   `talla` varchar(10) DEFAULT NULL,
   `color` varchar(50) DEFAULT NULL,
   `imagen` varchar(255) DEFAULT NULL,
+  `fecha_creacion` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `codigo_barras` (`codigo_barras`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +70,7 @@ CREATE TABLE `productos` (
 
 LOCK TABLES `productos` WRITE;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (1,'Remera básica blanca','Remeras',4999.99,17,'100001','M','Blanco','default.jpg'),(5,'Buzo oversize negro','Buzos',8999.99,15,'100002','L','Negro','default.jpg'),(6,'Campera de jean','Camperas',14999.00,10,'100003','M','Azul','default.jpg'),(7,'Pantalón cargo verde','Pantalones',11999.00,12,'100004','L','Verde','default.jpg'),(8,'Remera oversize gris','Remeras',5499.00,30,'100005','XL','Gris','default.jpg'),(9,'Campera rompevientos','Camperas',13999.00,8,'100006','S','Rojo','default.jpg'),(10,'Pantalón jogging negro','Pantalones',9999.00,18,'100007','M','Negro','default.jpg'),(11,'Buzo con capucha beige','Buzos',8999.00,14,'100008','L','Beige','default.jpg'),(12,'Remera estampada azul','Remeras',5999.00,22,'100009','S','Azul','default.jpg'),(13,'Campera inflable negra','Camperas',15999.00,9,'100010','XL','Negro','default.jpg'),(15,'pantalon gris','Pantalones',6999.99,6,'56877492','S','gris','producto-1748440981559.jpg');
+INSERT INTO `productos` VALUES (1,1,'Remera básica','Remeras',4999.99,15,'100001','M','Blanco','default.jpg','2025-06-06 11:10:16'),(5,2,'Buzo oversize negro','Buzos',8999.99,15,'100002','L','Negro','default.jpg','2025-06-06 11:10:16'),(6,3,'Campera de jean','Camperas',14999.00,10,'100003','M','Azul','default.jpg','2025-06-06 11:10:16'),(7,7,'Pantalón cargo verde','Pantalones',11999.00,12,'100004','L','Verde','default.jpg','2025-06-06 11:10:16'),(8,8,'Remera oversize gris','Remeras',5499.00,30,'100005','XL','Gris','default.jpg','2025-06-06 11:10:16'),(9,9,'Campera rompevientos','Camperas',13999.00,8,'100006','S','Rojo','default.jpg','2025-06-06 11:10:16'),(10,10,'Pantalón jogging','Pantalones',9999.00,18,'100007','M','Negro','default.jpg','2025-06-06 11:10:16'),(11,11,'Buzo con capucha beige','Buzos',8999.00,14,'100008','L','Beige','default.jpg','2025-06-06 11:10:16'),(12,12,'Remera estampada azul','Remeras',5999.00,22,'100009','S','Azul','default.jpg','2025-06-06 11:10:16'),(13,13,'Campera inflable negra','Camperas',15999.00,9,'100010','XL','Negro','default.jpg','2025-06-06 11:10:16'),(15,15,'pantalon gris','Pantalones',6999.99,6,'56877492','S','gris','producto-1748440981559.jpg','2025-06-06 11:10:16'),(16,1,'Remera básica','Remeras',4999.99,20,'10000001','S','Negro','default.jpg','2025-06-07 23:03:55'),(19,1,'Remera básica','Remeras',4999.99,23,'1354668','M','Negro','default.jpg','2025-06-07 23:22:57'),(23,13,'Campera inflable','Camperas',15999.00,13,'556468741','XL','Azul','default.jpg','2025-06-07 23:55:21');
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,7 +90,7 @@ CREATE TABLE `ventas` (
   PRIMARY KEY (`id`),
   KEY `producto_id` (`producto_id`),
   CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,9 +99,13 @@ CREATE TABLE `ventas` (
 
 LOCK TABLES `ventas` WRITE;
 /*!40000 ALTER TABLE `ventas` DISABLE KEYS */;
-INSERT INTO `ventas` VALUES (1,1,1,4999.99,'2025-05-28 18:59:45');
+INSERT INTO `ventas` VALUES (1,1,1,4999.99,'2025-05-28 18:59:45'),(2,1,2,4999.99,'2025-06-08 17:08:50');
 /*!40000 ALTER TABLE `ventas` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'tienda_ropa'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -85,4 +116,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-04 10:05:19
+-- Dump completed on 2025-06-09  9:27:21
